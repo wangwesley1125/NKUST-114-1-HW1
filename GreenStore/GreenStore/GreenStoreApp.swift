99079bfdd1f2aabbe3880f527db23b0339cbe55d
WesleyWang <wangwesley1125@gmail.com>
2025-11-09 00:37:56 +0800

    Add SwiftUI Code

---
+++ GreenStore/GreenStore/GreenStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  GreenStoreApp.swift
+//  GreenStore
+//
+//  Created by 王耀偉 on 2025/11/8.
+//
+
+import SwiftUI
+
+@main
+struct GreenStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GreenStore/GreenStoreTests/GreenStoreTests.swift
@@ -0,0 +1,17 @@
+//
+//  GreenStoreTests.swift
+//  GreenStoreTests
+//
+//  Created by 王耀偉 on 2025/11/8.
+//
+
+import Testing
+@testable import GreenStore
+
+struct GreenStoreTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
