6e4a4381a60b99c630b00125532b66a693a03621
WesleyWang <wangwesley1125@gmail.com>
2025-11-12 13:18:27 +0800

    Catch User Location

--- GreenStore/GreenStore/HomeView.swift
+++ GreenStore/GreenStore/HomeView.swift
@@ -10,15 +10,23 @@ import MapKit
 
 struct HomeView: View {
     
+    let cameraPosition: MapCameraPosition = .region(.init(center: .init(latitude: 22.654523735389255, longitude: 120.32997267412121), latitudinalMeters: 130, longitudinalMeters: 130))
+    
     let locationManager = CLLocationManager()
     
     var body: some View {
-        Map() {
+        Map(initialPosition: cameraPosition) {
             UserAnnotation()
         }
-        .onAppear {
-            locationManager.requestWhenInUseAuthorization()
-        }
+//        .onAppear {
+//            locationManager.requestWhenInUseAuthorization()
+//        }
+//        .mapControls {
+//            MapUserLocationButton()
+//            MapCompass()
+//            MapPitchToggle()
+//            MapScaleView()
+//        }
     }
     
 }
