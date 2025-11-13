864d3b199ed320bbe0d23f13b37037fb281436da
WesleyWang <wangwesley1125@gmail.com>
2025-11-13 21:32:39 +0800

    Test MapKit

--- GreenStore/GreenStore/HomeView.swift
+++ GreenStore/GreenStore/HomeView.swift
@@ -9,29 +9,9 @@ import SwiftUI
 import MapKit
 
 struct HomeView: View {
-    // 以台中市政府為中心
-    @State private var cameraPosition: MapCameraPosition = .region(
-        .init(center: .init(latitude: 24.16304671963227,
-                            longitude: 120.64669143694526),
-              latitudinalMeters: 1300,
-              longitudinalMeters: 1300)
-    )
-    
-    let locationManager = CLLocationManager()
     
     var body: some View {
-        Map(position: $cameraPosition) {
-            UserAnnotation()
-        }
-//        .onAppear {
-//            locationManager.requestWhenInUseAuthorization()
-//        }
-//        .mapControls {
-//            MapUserLocationButton()
-//            MapCompass()
-//            MapPitchToggle()
-//            MapScaleView()
-//        }
+        Map()
     }
     
 }
